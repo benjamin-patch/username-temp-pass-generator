d1fc39b29abcc1f9df6e5b8cfa9c2a4def23f5bc
Benjamin Patch <ben@benjaminpatch.com>
2025-03-05 22:38:11 -0800

    Refactor so the only inputs required are first and last name.

--- generator_script.py
+++ generator_script.py
@@ -18,12 +18,34 @@ def password_generator(user_name):
   password = last_char + remaining_chars
   return password
 
-# inputs
-print(username_generator('Pepper', 'Jack'))
-print(password_generator('PepJack'))
+# input
 
-print(username_generator('Dr', 'Wei'))
-print(password_generator('DrWei'))
+# user 1
+user1_fname = 'Pepper'
+user1_lname = 'Jack'
+user1_username = username_generator(user1_fname, user1_lname)
+user1_temp_pass = password_generator(user1_username)
+# user 2
+user2_fname = 'Dr'
+user2_lname = 'Wei'
+user2_username = username_generator(user2_fname, user2_lname)
+user2_temp_pass = password_generator(user2_username)
+# user 3
+user3_fname = 'Abe'
+user3_lname = 'Simpson'
+user3_username = username_generator(user3_fname, user3_lname)
+user3_temp_pass = password_generator(user3_username)
 
-print(username_generator('Abe', 'Simpson'))
-print(password_generator('AbeSimp'))
+# output
+
+# user 1
+print('Username:', user1_username)
+print('Temp Password:', user1_temp_pass)
+
+# user 2
+print('Username:', user2_username)
+print('Temp Password:', user2_temp_pass)
+
+# user 3
+print('Username:', user3_username)
+print('Temp Password:', user3_temp_pass)

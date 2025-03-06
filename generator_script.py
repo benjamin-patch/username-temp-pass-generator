c8d211af62de9368395e344a55b0bcdff20f4c5e
Benjamin Patch <ben@benjaminpatch.com>
2025-03-05 22:02:07 -0800

    Write 'password_generator()' function with inputs for testing.

--- generator_script.py
+++ generator_script.py
@@ -1,3 +1,4 @@
+# generate username
 def username_generator(first_name, last_name):
   if len(first_name) >= 3:
     user_fname = first_name[0:3]
@@ -10,9 +11,19 @@ def username_generator(first_name, last_name):
   user_name = user_fname + user_lname
   return user_name
 
-def password_generator():
-  pass
+# generate password
+def password_generator(user_name):
+  last_char = user_name[-1]
+  remaining_chars = user_name[:-1]
+  password = last_char + remaining_chars
+  return password
 
-# output
+# inputs
 print(username_generator('Pepper', 'Jack'))
+print(password_generator('PepJack'))
+
 print(username_generator('Dr', 'Wei'))
+print(password_generator('DrWei'))
+
+print(username_generator('Abe', 'Simpson'))
+print(password_generator('AbeSimp'))
